5ef9a73da596905be1c5c43feb29aaba11a54fb6
Narmes <@horux.08>
2023-09-17 20:23:45 -0500

    app

---
+++ config-env.ts
@@ -0,0 +1,25 @@
+const fs = require('fs');
+require('dotenv').config();
+const { argv } = require('yargs');
+const environment = argv.environment;
+
+let apiKey;
+let targetPath;
+if (environment === 'prod') {
+    apiKey = process.env.unplashKey;
+    targetPath = `./src/environments/environment.prod.ts`;
+} else {
+    apiKey = process.env.unplashKey;
+    targetPath = `./src/environments/environment.ts`;
+}
+
+const envConfigFile = `
+export const environment = {
+    production: false,
+    unplashKey: "${apiKey}",};`;
+
+fs.writeFile(targetPath, envConfigFile, function (err: any) {
+    if (err) {
+        console.log(err);
+    }
+});
---
+++ src/app/components/components-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UnplashImgComponent } from './unplash-img/unplash-img.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    children:[
+      {
+        path:'searchImg', component:UnplashImgComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ComponentsRoutingModule { }
