71bf2027f053a8bd0f0a7e39c8bed28e32efe0b4
Narmes <@horux.08>
2023-09-17 21:07:52 -0500

    js

---
+++ config-env.js
@@ -0,0 +1,23 @@
+const setEnv = () => {
+  const fs = require('fs');
+  const writeFile = fs.writeFile;
+  const targetPath = './src/environments/environment.ts';
+  const colors = require('colors');
+  require('dotenv').config({
+    path: 'src/environments/.env'
+  });
+  // `environment.ts` file structure
+  const envConfigFile = `export const environment = {
+  unplashKey: '${process.env.unplashKey}',
+  production: true,
+  };
+  `;
+
+  writeFile(targetPath, envConfigFile, (err) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+  });
+
+  setEnv();}
