23ef4f91e4218ea1e0739d61e8345d1070551ac0
Narmes <@horux.08>
2023-09-17 22:14:11 -0500

    env

--- config-env.js
+++ config-env.js
@@ -1,7 +1,7 @@
 const setEnv = () => {
   const fs = require('fs');
   const writeFile = fs.writeFile;
-  const targetPath = './src/environments/environment.ts';
+  const targetPath = './src/environments/environment';
   const colors = require('colors');
   require('dotenv').config({
     path: 'src/environments/.env'
