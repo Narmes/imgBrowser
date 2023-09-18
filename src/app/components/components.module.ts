ffef0cbb10274578d14aedca4a07bfcaf3289b17
Narmes <@horux.08>
2023-09-18 01:07:19 -0500

    det

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { ComponentsRoutingModule } from './components-routing.module';
 import { UnplashImgComponent } from './unplash-img/unplash-img.component';
@@ -9,6 +10,7 @@ import { UnplashImgComponent } from './unplash-img/unplash-img.component';
     UnplashImgComponent
   ],
   imports: [
+    CommonModule,
     ComponentsRoutingModule,
   ]
 })
